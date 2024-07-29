4da997c8f3e6eb590e0a50d55cccac2b6175e38e
katniny <okuwu1680@gmail.com>
2024-07-29 17:56:41 -0600

    Added animation when closing a modal, also fixed an issue where users could close modals with the ESC key

--- assets/js/ts_fas_acih.js
+++ assets/js/ts_fas_acih.js
@@ -6469,3 +6469,37 @@ firebase.auth().onAuthStateChanged((user) => {
       }
    }
 })
+
+// prevent .close(); from closing a modal without an animation
+// save the original close method ofc tho
+const originalClose = HTMLDialogElement.prototype.close;
+
+HTMLDialogElement.prototype.close = function() {
+   const modal = this;
+
+   // apply the animation
+   modal.style.animation = "closePopup 0.5s ease";
+
+   // wait for the animation to finish before actually closing the modal
+   setTimeout(() => {
+      // call the original close method
+      originalClose.call(modal);
+
+      // remove the animation
+      modal.style.animation = "";
+   }, 100);
+};
+
+// prevent modals from being closed with esc as it can break things (such as 
+// letting the user use transsocial without verifying their email... oopsies)
+// this is just default <dialog> behavior, not something i manually coded :p
+document.addEventListener("keydown", function(event) {
+   if (event.key === "Escape") {
+      const openDialog = document.querySelector("dialog[open]");
+      if (openDialog) {
+         // this will prevent it
+         event.preventDefault();
+         event.stopPropagation(); 
+      }
+   }
+});
