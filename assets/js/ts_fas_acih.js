6cdf493986606f23d0f90071f87355d079d7cc68
Luna B <lunab08@proton.me>
2024-07-29 17:55:52 +0200

    update updates and bump version

--- assets/js/ts_fas_acih.js
+++ assets/js/ts_fas_acih.js
@@ -26,7 +26,7 @@ const pathName = pageURL.pathname;
 let isOnDesktopApp = null;
 
 // TransSocial Version
-let transsocialVersion = "v2024.7.28";
+let transsocialVersion = "v2024.7.29";
 let transsocialReleaseVersion = "indev";
 
 const notices = document.getElementsByClassName("version-notice");
