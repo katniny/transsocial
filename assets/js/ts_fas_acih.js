aa9fc5bfd6b76cb9668ab58d7b239aa55b659640
katniny <110915403+katniny@users.noreply.github.com>
2024-12-05 20:59:54 -0700

    Bump version

--- assets/js/ts_fas_acih.js
+++ assets/js/ts_fas_acih.js
@@ -29,8 +29,8 @@ const pathName = pageURL.pathname;
 let isOnDesktopApp = null;
 
 // TransSocial Version
-let transsocialVersion = "v2024.12.1";
-let transsocialUpdate = "v2024121-2";
+let transsocialVersion = "v2024.12.5";
+let transsocialUpdate = "v2024125-1";
 let transsocialReleaseVersion = "pre-alpha";
 
 const notices = document.getElementsByClassName("version-notice");
