faff49d6f2a1982e855b19fbca367ad6affa0625
katniny <okuwu1680@gmail.com>
2025-01-12 06:20:57 -0700

    Add the not signed in banner

--- assets/js/getLoggedInUser.js
+++ assets/js/getLoggedInUser.js
@@ -7,5 +7,15 @@ firebase.auth().onAuthStateChanged((user) => {
    } else {
       isSignedIn = false;
       console.log(`Signed in: ${isSignedIn}`);
+
+      // prompt user to login
+      const notSignedInbanner = document.createElement("div");
+      notSignedInbanner.setAttribute("id", "notSignedIn-banner");
+      notSignedInbanner.innerHTML = `
+         <p>Join the fun on TransSocial!</p>
+         <a href="/auth/login"><button>Login</button></a>
+         <a href="/auth/register"><button>Create an account</button></a>
+      `;
+      document.body.appendChild(notSignedInbanner);
    }
 });
