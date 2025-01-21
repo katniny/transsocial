2a52adf712ce210445917961fc1d0b0d908e824c
katniny <okuwu1680@gmail.com>
2025-01-20 22:01:35 -0700

    Server return UID (even if UID provided)

--- functions/server/fetch-user-from-id.js
+++ functions/server/fetch-user-from-id.js
@@ -71,7 +71,10 @@ app.get("/", async (req, res) => {
 
       const userData = snapshot.val();
 
-      return res.status(200).send(userData);
+      return res.status(200).send({
+         uid: req.query.id,
+         ...userData
+      });
    } catch (error) {
       console.error("Error fetching user: ", error);
       return res.status(500).send({ error: "Internal server error." });
